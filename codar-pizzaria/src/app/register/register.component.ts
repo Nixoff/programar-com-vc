501ffd31a8abe966fb71e7678e38a0d72fadf7b1
Ivan Vidal <ivanvalmeida.123@gmail.com>
2020-10-24 13:41:20 -0300

    register-service create

--- codar-pizzaria/src/app/register/register.component.ts
+++ codar-pizzaria/src/app/register/register.component.ts
@@ -1,25 +1,37 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { AppRoutingModule }  from '../app-routing.module'
-
-
-
-
+import { Component, OnInit } from '@angular/core';
+import { RegisterService } from '../register.service';
+import { FormGroup, FormBuilder  } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-@Injectable({
-  providedIn: 'root'
-})
 export class RegisterComponent implements OnInit {
-
  
+  loginForm: FormGroup;
+  submit: boolean;
 
-  constructor() { }
+  constructor(private _register: RegisterService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-   
+    this.loginForm = this.fb.group({
+      'name': ['', ], 
+      'email': ['', ],
+      'password': ['', ],
+      'confirma_password': ['', ],
+    });
   }
 
+  login(){
+    console.log('Cadastro em progresso')
+    this.submit = true;
+    this._register.login(this.loginForm.value);
+  }
+
+  usuarioCadastrado(): boolean {
+    if(localStorage.getItem('userLogado')){
+      return true;
+    }
+    return false;
+  }
 }
